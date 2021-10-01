48ec7f2d41f70caed3ae2e62ab752f06679de195
bridgerz <bridgerzoske@gmail.com>
2021-10-01 13:07:50 -0700

    Add search copy

--- packages/react-app/src/pages/business/components/BusinessSearchBar.tsx
+++ packages/react-app/src/pages/business/components/BusinessSearchBar.tsx
@@ -1,5 +1,5 @@
 import { BoxProps } from "@chakra-ui/layout"
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
+import { Input, InputGroup, VStack, Text, InputLeftElement } from "@chakra-ui/react"
 import { faAt } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React, { useRef } from "react"
@@ -17,12 +17,15 @@ const BusinessSearchBar = ({ onSearch, ...rest }: Props) => {
   }
 
   return (
-    <InputGroup>
-      <InputLeftElement zIndex={1} pointerEvents="none">
-        <FontAwesomeIcon icon={faAt} />
-      </InputLeftElement>
-      <Input ref={inputRef} onKeyPress={handleKeyPressed} placeholder={`Search businesses`} />
-    </InputGroup>
+    <>
+      <InputGroup>
+        <InputLeftElement zIndex={1} pointerEvents="none">
+          <FontAwesomeIcon icon={faAt} />
+        </InputLeftElement>
+        <Input ref={inputRef} onKeyPress={handleKeyPressed} placeholder={`Search businesses`} />
+      </InputGroup>
+      <Text>business handle must be exact</Text>
+    </>
   )
 }
 

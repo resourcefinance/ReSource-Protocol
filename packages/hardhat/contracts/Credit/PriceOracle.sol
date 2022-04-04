6681edc75c83a2a171d1fd61332958d166b7815d
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-04-04 11:30:44 -0700

    Update PriceOracle.sol

--- packages/hardhat/contracts/Credit/PriceOracle.sol
+++ packages/hardhat/contracts/Credit/PriceOracle.sol
@@ -2,20 +2,30 @@
 pragma solidity ^0.8.0;
 
 import "./interface/IPriceOracle.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PriceOracle is IPriceOracle {
+contract PriceOracle is IPriceOracle, Ownable {
     uint256 public price;
-    uint32 private constant MIN_PPT = 1000;
+    address public oracleManager;
 
     constructor(uint256 _price) {
         price = _price;
     }
 
-    function setPrice(uint256 _price) external {
+    function setPrice(uint256 _price) external onlyOracleManager {
         price = _price;
     }
 
     function getPriceInPPT() external view override returns (uint256) {
         return price;
+    }
+
+    function setOracleManager(address _oracleManager) external onlyOwner {
+        oracleManager = _oracleManager;
+    }
+
+    modifier onlyOracleManager() {
+        require(msg.sender == oracleManager, "PriceOracle: Caller must be manager.");
+        _;
     }
 }
